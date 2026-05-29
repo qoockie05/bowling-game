21c330944c9596d061b75dcd1fc0b0fc992fa901
qoockie05 <qoockie05@gmail.com>
2026-05-29 17:15:21 +0200

    iteration 2 - added allRollsHitTwoPinsShouldScore40

--- src/test/java/tdd/BowlingGameTest.java
+++ src/test/java/tdd/BowlingGameTest.java
@@ -7,13 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class BowlingGameTest {
 
     private Game g;
-    @BeforeEach
-    void setUp() {
-        g = new Game();
-    }
+
     @Test
     void worstGameShouldScoreZero() {
         for (int i = 0; i < 20; i++) g.roll(0);
         assertEquals(0, g.score());
+    }
+    @Test
+    void allRollsHitTwoPinsShouldScore40() {
+        for (int i = 0; i < 20; i++) g.roll(2);
+        assertEquals(40, g.score());
     }
 }
