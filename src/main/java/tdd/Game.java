289598066af6764391f7ee78a0d02f144541d0e0
qoockie05 <qoockie05@gmail.com>
2026-05-29 17:28:57 +0200

    iteration 2 - all rolls same number of pins

--- src/main/java/tdd/Game.java
+++ src/main/java/tdd/Game.java
@@ -2,11 +2,17 @@ package tdd;
 
 
 public class Game {
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
+        rolls[currentRoll++] = pins;
     }
-
     public int score() {
-        return 0;
+        int score = 0;
+        for (int i = 0; i < 20; i++) {
+            score += rolls[i];
+        }
+        return score;
     }
 }
--- src/test/java/tdd/BowlingGameTest.java
+++ src/test/java/tdd/BowlingGameTest.java
@@ -10,11 +10,13 @@ class BowlingGameTest {
 
     @Test
     void worstGameShouldScoreZero() {
+        Game g = new Game();
         for (int i = 0; i < 20; i++) g.roll(0);
         assertEquals(0, g.score());
     }
     @Test
     void allRollsHitTwoPinsShouldScore40() {
+        Game g = new Game();
         for (int i = 0; i < 20; i++) g.roll(2);
         assertEquals(40, g.score());
     }
